c2ddd12c48ed98f847e256cf0b1c97878f6d084e
Nada Ayman <naymanallam@gmail.com.com>
2023-04-20 05:38:54 +0200

    Adding Day2 Solution

---
+++ MVC/Day2/Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using Day2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Show(int id, string name, string Image)
+        {
+            Image image = new Image() { ID = id, Name = name, URL = Image };
+            ViewBag.Image = image;
+            return View();
+        }
+    }
+}
---
+++ MVC/Day2/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day2.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+    }
+}
