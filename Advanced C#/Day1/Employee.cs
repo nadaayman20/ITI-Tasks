e2ccf055509e6ecd79001093fb5bb8e013873968
Nada Ayman <naymanallam@gmail.com.com>
2023-04-23 01:13:43 +0200

    Adding Day1 Solution

---
+++ Advanced C#/Day1/Employee.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Day1
+
+//{
+//    public enum Gender
+//    {
+//        Male, Female
+//    };
+//    public enum Securitylevel
+//    {
+//        Guest,
+//        Developer,
+//        Secretary,
+//        DBA
+//    };
+
+//    struct HireDate
+//    {
+//        int day;
+//        int month;
+//        int year;
+
+//        public HireDate(int _day, int _month, int _year)
+//        {
+//            this.day = _day;
+//            this.month = _month;
+//            this.year =_year;
+
+//        }
+//        public int GetDay()
+//        {
+//            return day;
+//        }
+//        public void SetDay(int day)
+//        {
+//            this.day = day;
+//        }
+
+//        public int GetMonth()
+//        {
+//            return month;
+//        }
+//        public void SetMonth(int month)
+//        {
+//            this.month = month;
+//        }
+
+//        public int GetYear()
+//        {
+//            return year;
+//        }
+//        public void SetYear(int year)
+//        {
+//            this.year = year;
+//        }
+//    }
+//    internal class Employee:IComparable
+//    {
+//        private int id;
+//        private string name;
+//        private Securitylevel securitylevel;
+//        private Gender gender;
+//        private float salary;
+//        private HireDate date;
+
+//        public Employee(int _id,string _name,Securitylevel _securty, Gender _gen, float _salary ,HireDate _date)
+//        {
+//            this.id = _id;
+//            this.name = _name;
+//            this.securitylevel = _securty;
+//            this.gender = _gen;
+//            this.salary = _salary;
+//            this.date = _date;
+
+//        }
+//        #region Property
+
+//        public HireDate Hiredate
+//        {
+//            get
+//            {
+//                return date;
+//            }
+//            set
+//            {
+//                date = value;
+//            }
+//        }
+//        public int ID
+//        {
+//            get
+//            {
+//                return id;
+//            }
+//            set
+//            {
+//                if (value >= 1000)
+//                {
+//                    id = value;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Enter Valid ID");
+//                }
+//            }
+//        }
+//        public string Name
+//        {
+//            get
+//            { return name; }
+//            set
+//            {
+//                if (value != null)
+//                {
+//                    name = value;
+//                }
+//            }
+//        }
+
+//        public Securitylevel Securitylevel
+//        {
+//            get
+//            {
+//                return securitylevel;
+//            }
+//            set
+//            {
+
+//                securitylevel = value;
+//            }
+
+//        }
+
+//        public float Salary
+//        {
+//            get
+//            {
+//                return salary;
+//            }
+//            set
+//            {
+//                if (value >= 3500)
+//                {
+//                    salary = value;
+//                }
+//                else
+//                    Console.WriteLine("Enter Valid Salary  ");
+//            }
+//        }
+
+
+//        public Gender Gender
+//        {
+//            get
+//            {
+//                return gender;
+//            }
+//            set
+//            {
+//                gender = value;
+//            }
+
+//        }
+//        public int CompareTo(object obj)
+//        {
+//            Employee e = obj as Employee;
+           
+//        }
+
+//        public void ShowInfo()
+//        {
+//            Console.Write($"ID:{id}\tName:{name}\tSalary:{salary}\tGender:{gender}\tHiringDate: {date.GetDay()}-{date.GetMonth()}-{date.GetYear()}\t\tSecurityLevel:{securitylevel}\n");
+
+//        }
+//         public override string ToString() {
+//            return String.Format($"{name} is taking {salary}");
+//        }
+
+//    }
+//}
+
---
+++ Advanced C#/Day1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size1;
+            Console.WriteLine("Please Enter Num of student");
+            size1 = int.Parse(Console.ReadLine());
+            int size2;
+            Console.WriteLine("Please Enter Num of student");
+            size2 = int.Parse(Console.ReadLine());
+            int[,] arr = new int[size1, size2];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    int grade;
+                    do
+                    {
+                        Console.WriteLine("Enter Your Grade is ");
+                        grade = int.Parse(Console.ReadLine());
+
+                    } while (grade == null);
+
+                    arr[i, j] = grade;
+                }
+            }
+
+                 int sum;
+                float avg ;
+                int rows=arr.GetLength(0);
+                int cols=arr.GetLength(1);
+
+            //Sum of rows
+            for(int i=0; i<rows; i++)
+            {
+                sum= 0;
+                for(int j=0;j<cols; j++)
+                {
+                    sum +=  arr[i, j];
+                }
+               
+                Console.WriteLine("Sum of " + (i + 1) + " row: " + sum);
+
+            }
+
+            //Average of Columns
+            for (int i = 0; i < cols; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < rows; j++)
+                {
+                    sum  += arr[j, i];
+                }
+                //Console.WriteLine("Sum of " + (i + 1) + " column: " + sum);
+                avg=sum/rows;
+                Console.WriteLine("Average of  " + (i + 1) + " column: " + avg);
+            }
+
+
+
+            /////////// Employee
+           
+         
+        }
+    }
+ }
