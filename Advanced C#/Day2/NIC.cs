cdb3eeec512dd0453294ca4dd7aaa318d0001b4a
Nada Ayman <naymanallam@gmail.com.com>
2023-04-23 01:14:44 +0200

    Adding Day2 Solution

---
+++ Advanced C#/Day2/NIC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    public enum Type
+    {
+        Ethernet , token
+    }; 
+
+    public class NIC
+    {
+       private StringBuilder Manufacture;
+       private StringBuilder MAC_Address;
+       private Type type;
+
+        public NIC() {
+           Manufacture =new StringBuilder("Device") ;
+           MAC_Address =new StringBuilder("00-B0-D0-63-C2-26") ;
+           type = Type.Ethernet ;
+         }
+
+
+        public static NIC CreateObj { get; }  = new NIC();
+
+        public void printData()
+        {
+            Console.WriteLine($"Manufacture is {Manufacture} and MAC Address is{MAC_Address} and Type is {type} ");
+        }
+    }
+
+}
---
+++ Advanced C#/Day2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine($"Enter The Size of Array ");
+            size = int.Parse( Console.ReadLine() );
+
+            int[] arr= new int[size];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Enter The value of Array {i} from {size}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int max = 0;
+            int num1 = 0;
+            int num2 = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        num1 = j;
+                        num2 = i;
+                    }   
+                    
+                    if (num1 - num2 > max)
+                    {
+                        max = num1 - num2;
+                    }
+                 
+                }
+              
+            }
+            Console.WriteLine($"the longest distance between  them is {max-1} cells");
+
+            Console.WriteLine();
+            Console.WriteLine("==========================");
+
+            ////// Task 2
+           
+            NIC n1 = NIC.CreateObj;
+            n1.printData();
+            NIC n2 = NIC.CreateObj;
+            n2.printData();
+
+            Console.WriteLine() ;
+
+            Console.WriteLine(n1.GetHashCode());
+            Console.WriteLine(n2.GetHashCode());
+
+        }
+    }
+}
