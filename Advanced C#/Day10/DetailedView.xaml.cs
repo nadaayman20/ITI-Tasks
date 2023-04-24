45b9960c0934740b2eebaa3570e8116fd6fb1027
Nada Ayman <naymanallam@gmail.com.com>
2023-04-24 15:01:29 +0200

    Adding Day10 Solution

---
+++ Advanced C#/Day10/DetailedView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Day_10.Entities;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Day_10
+{
+    /// <summary>
+    /// Interaction logic for DetailedView.xaml
+    /// </summary>
+    public partial class DetailedView : Window
+    {
+        pubsContext Context;
+
+        public DetailedView()
+        {
+            Context = new pubsContext();
+            InitializeComponent();
+            Closing += (sender, e) => Context.Dispose();
+            Load();
+        }
+
+        private void Load()
+        {
+            if (!productList.Items.IsEmpty)
+                productList.ItemsSource = null;
+            Context.titles.Load();
+            Context.publishers.Load();
+            productList.ItemsSource = Context.titles.Local.ToBindingList();
+            productList.DisplayMemberPath = "title1";
+            productList.SelectedValuePath = "title_id";
+            productList.SelectionChanged += (sender, e) => StackPanelCustom.DataContext = productList.SelectedItem;
+            ProductID.SetBinding(ContentProperty, new Binding("title_id"));
+            ProductName.SetBinding(TextBox.TextProperty, new Binding("title1"));
+            ProductPrice.SetBinding(TextBox.TextProperty, new Binding("price"));
+            box.ItemsSource = Context.publishers.Local.ToBindingList();
+            box.SelectedValuePath = "pub_id";
+            box.DisplayMemberPath = "pub_name";
+            box.SetBinding(ComboBox.SelectedValueProperty, new Binding("pub_id"));
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Advanced C#/Day10/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Day_10.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         pubsContext Context ;
+
+        public MainWindow()
+        {
+            Context = new pubsContext();
+            InitializeComponent();
+            Closing += (sender, e) => Context.Dispose();
+        }
+
+        private void DataLoaded(object sender, RoutedEventArgs e)
+        {
+            DataGridComboBoxColumn comboBox = new();
+            comboBox.SelectedValueBinding = new Binding("pub_id");
+            Context.titles.Load(); 
+            Context.publishers.Load();
+            comboBox.ItemsSource = Context.publishers.Local.ToBindingList();
+            comboBox.Header = "Publisher Header";
+            comboBox.DisplayMemberPath = "pub_name";
+            comboBox.SelectedValuePath = "pub_id";
+            
+            data.ItemsSource = Context.titles.Local.ToBindingList();
+            data.Columns.ElementAt(3).IsReadOnly = true;
+            data.Columns.ElementAt(3).Header = "Publisher";
+            data.Columns.ElementAt(1).Width = 150;
+            data.Columns.ElementAt(3).Width = 80;
+            data.Columns.ElementAt(4).Width = 80;
+            data.Columns.ElementAt(6).Width = 80;
+            data.Columns.ElementAt(7).Width = 80;
+            data.Columns.ElementAt(8).Width = 130;
+            data.Columns.RemoveAt(10);
+            data.Columns.Add(comboBox);
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            Context.SaveChanges();
+        }
+    }
+}
