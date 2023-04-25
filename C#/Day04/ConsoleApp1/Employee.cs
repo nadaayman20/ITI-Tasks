6c37dc4cb924c22bb7866afac3a0abdad845a430
Nada Ayman <naymanallam@gmail.com.com>
2023-04-25 20:14:57 +0200

    Adding Day04 Solution

---
+++ C#/Day04/ConsoleApp1/Employee.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    enum SecurityLevel
+    {
+        guest,
+        developer,
+        secretary,
+        DPA
+    }
+    enum Gender
+    {
+        M,
+        F
+    }
+    struct HireDate
+    {
+        int Day;
+        int Month;
+        int Year;
+
+        public void SetDay(int day)
+        {
+            if (day > 0)
+                Day = day;
+        }
+        public int GetDay() {
+            return Day;
+        public void SetMonth(int month)
+        {
+            Month = month;
+        }
+        public int GetMonth()
+        { return Month; }
+        public void SetYear(int year)
+        {
+            Year = year;
+        }
+        public int GetYear()
+        { return Year; }
+
+        public HireDate(int day,int month,int year)
+        {
+            Day= day;
+            Month = month;
+            Year = year;
+        }
+    }
+    struct Employees
+    {
+        int Id;
+        SecurityLevel securitylevel;
+        float Salary;
+        HireDate hiredate;
+        Gender gender;
+
+        public void SetId(int id)
+        {
+            if (id > 0)
+                Id = id;
+            else
+                Console.WriteLine("Pleas enter correct Id");
+        }
+        public int GetId() {
+               return Id;
+
+        public void SetSecurityLevel(SecurityLevel securlevel)
+        {
+            securitylevel = securlevel;
+        }
+        public SecurityLevel GetSecurityLevel()
+                return securitylevel;
+
+        public void SetSalary(float salary)
+        {
+            if (salary >= 3500)
+                Salary = salary;
+        }
+        public float GetSalary() {
+                return Salary;
+             }
+
+        public void SetHireDate(HireDate date)
+        {
+            hiredate = date;
+        }
+        public HireDate GetHireDate() {
+                return hiredate;
+           }
+
+        public void SetGender(Gender gen)
+        {
+            gender = gen;
+        }
+        public Gender GetGender() {
+                return gender;
+           }
+        public Employees(int id, float salary, HireDate hire, Gender g, SecurityLevel level)
+        {
+            Id = id;
+            Salary = salary;
+            hiredate = hire;
+            gender = g;
+            securitylevel = level;
+
+        }
+
+
+    }
+}
---
+++ C#/Day04/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Employee;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees[] arr = new Employees[1];
+            for(int i=0; i<arr.Length;i++)
+            {
+                Console.WriteLine("Enter ID");
+                int id=int.Parse(Console.ReadLine());
+                arr[i].SetId(id);
+
+                Console.WriteLine("Enter security level");
+               SecurityLevel level = (SecurityLevel)Enum.Parse(typeof(SecurityLevel), Console.ReadLine());
+                arr[i].SetSecurityLevel(level);
+
+                Console.WriteLine("Enter salary");
+                float salary = float.Parse(Console.ReadLine());
+                arr[i].SetSalary(salary);
+
+                HireDate date= new HireDate();
+                Console.WriteLine("Enter hire date");
+                int day=int.Parse(Console.ReadLine());
+                int month = int.Parse(Console.ReadLine());
+                int year = int.Parse(Console.ReadLine());
+                date.SetDay(day);
+                date.SetMonth(month);
+                date.SetYear(year);
+
+                Console.WriteLine("Enter gender");
+                Gender gender= (Gender) Enum.Parse(typeof(Gender), Console.ReadLine());
+                arr[i].SetGender(gender);
+
+            }
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                Console.WriteLine($"Id :{arr[j].GetId}");
+                Console.WriteLine($"Hiredate :{arr[j].GetHireDate}");
+                Console.WriteLine($"Salary :{arr[j].GetSalary}");
+                Console.WriteLine($"Gender :{arr[j].GetGender}");
+                Console.WriteLine($"security level :{arr[j].GetSecurityLevel}");
+            }
+
+        }
+    }
+}
