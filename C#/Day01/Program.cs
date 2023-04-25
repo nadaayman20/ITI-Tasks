d6da9b4b3e5aabc98f411ee3a89ed5a130590be8
Nada Ayman <naymanallam@gmail.com.com>
2023-04-25 20:13:30 +0200

    Adding Day01 Solution

---
+++ C#/Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers\n");
+
+            Console.WriteLine("Enter First Number ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = " + ( num1 + num2));
+            Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
+            Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
+            Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
+
+
+        
+            //Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            //Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            //Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            //Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+
+        }
+    }
+}
