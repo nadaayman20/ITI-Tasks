c1466addcb08a976d30380f670db1f6accc064da
Nada Ayman <naymanallam@gmail.com.com>
2023-04-26 19:29:06 +0200

    Adding Day05 Solution

---
+++ C#/Day05/Employee.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    public enum Gender {
+        Male, Female
+    };
+    public enum Securitylevel {
+        Guest,
+        Developer, 
+        Secretary, 
+        DBA 
+    };
+
+    struct HireDate
+    {
+        int day;
+        int month;
+        int year;
+
+        public HireDate(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+
+        }
+        public int GetDay()
+        {
+            return day;
+        }
+        public void SetDay(int day)
+        {
+            this.day = day;
+        }
+
+        public int GetMonth()
+        {
+            return month;
+        }
+        public void SetMonth(int month)
+        {
+            this.month = month;
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+        public void SetYear(int year)
+        {
+            this.year = year;
+        }
+    }
+    internal class Employee
+    {
+        private int id;
+        private Securitylevel securitylevel;
+        private Gender gender;
+        private float salary;
+        private HireDate date;
+
+        #region Property
+
+        public HireDate Hiredate
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value >= 1000)
+                {
+                    id = value;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid ID");
+                }
+            }
+        }
+
+        public Securitylevel Securitylevel
+        {
+            get
+            {
+                return securitylevel;
+            }
+            set
+            {
+
+                securitylevel = value;
+            }
+
+        }
+
+        public float Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value >= 3500)
+                {
+                    salary = value;
+                }
+                else
+                    Console.WriteLine("Enter Valid Salary  ");
+            }
+        }
+
+
+        public Gender Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.Write($"ID:{id}\tSalary:{salary}\tGender:{gender}\tHiringDate: {date.GetDay()}-{date.GetMonth()}-{date.GetYear()}\t\tSecurityLevel:{securitylevel}\n");
+
+        }
+  
+    }
+}
+
---
+++ C#/Day05/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Day05
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            String fulldate;
+            string idstr, strseclev, gender;
+            
+
+            Employee[] employees = new Employee[3];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = new Employee();
+
+                int id;
+                do
+                {
+                    Console.Write("Please Enter id:");
+                    idstr = Console.ReadLine();
+                   
+                }
+                while (!(int.TryParse(idstr, out id)));
+
+                employees[i].ID(id);
+
+                Securitylevel level;
+                do
+                {
+                    Console.WriteLine("Please Enter Security Level");
+                    strseclev = Console.ReadLine();
+                   
+                }
+                while (!Enum.TryParse(strseclev,true, out level));
+
+                employees[i].Securitylevel(level);
+
+                do
+                {
+                    Gender gen;
+                    Console.WriteLine("Please Enter Gender");
+                    gender = Console.ReadLine();
+                } while (!Enum.TryParse(gender, true, out gen));
+
+                employees[i].Gender(gen);
+                do {
+                    int day, month, year;
+                    string[] dateArr;
+                    do
+                {
+                        
+                        Console.Write("Please Enter the Date: ");
+
+                    fulldate = Console.ReadLine();
+
+                        dateArr = fulldate.Split(' ', '/', '-');
+                }
+
+                while (dateArr.Length != 3);
+
+                
+                        day = int.Parse(dateArr[0]);
+                        month = int.Parse(dateArr[1]);
+                        year = int.Parse(dateArr[2]);
+
+                    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2015)
+                        Console.WriteLine("Please Enter a Valid Date");
+
+                } while (day < 1 || day > 31 || month < 1 || month > 12 || year < 2015);
+
+                HireDate date = new HireDate(day, month, year);
+                employees[i].Hiredate(date);
+            }
+            for (int i = 0; i <= employees.Length; i++)
+            {
+                Console.WriteLine($"Id is {employees[i].ID()}");
+                Console.WriteLine($"Security Level is {employees[i].Securitylevel()}");
+                Console.WriteLine($"Gender is {employees[i].Gender()}");
+                Console.WriteLine($"Hire Date is {employees[i].Hiredate.GetDay()}/{employees[i].Hiredate.GetMonth()}/{employees[i].Hiredate.GetYear()}");
+
+            }
+
+        }
+    }
+} 
